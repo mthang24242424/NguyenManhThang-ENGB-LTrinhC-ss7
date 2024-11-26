e1690960056cefc8f9263ecdb8afc505812d8b3b
mthang24242424 <wunkevan@gmail.com>
2024-11-26 21:29:37 +0700

    ss7ex4

---
+++ ss7ex4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int numbers[n];
+	printf("nhap so phan tu cua mang ");
+	scanf("%d",&n);
+	numbers[n];
+	for(int i=0; i<n ; i++){
+		printf("nhap phan tu thu %d cho mang ",i);
+		scanf("%d",&numbers[i]);
+	}
+	return 0;
+}
