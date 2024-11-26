2633c039e271a8788b3ca151422ed78d3901bee5
mthang24242424 <wunkevan@gmail.com>
2024-11-26 21:29:53 +0700

    ss7ex5

---
+++ ss7ex5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	int numbers[5]={2,1,-4,9,7};
+	printf("gia tri lon nhat cua mang la %d ",numbers[3]);
+	printf("\n gia tri nho nhat cua mang la %d ",numbers[2]); 
+	return 0; 
+} 
